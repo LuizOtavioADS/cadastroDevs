2a6cd161bacb7bfe93eeaaf68f9fab381fa97fe2
LuizOtavioADS <luizo7avio7@gmail.com>
2024-03-14 23:18:45 -0300

    finally
    end

--- index.js
+++ index.js
@@ -52,12 +52,62 @@ addTechBtn.addEventListener('click', function(ev){
 
     //Criando o novo input, utilizando a função criada lá no início do código
     const techNameInput = createInput('techName- ' + rowIndex, null, 'techName')
+
+    //Criando as entradas de valores da tecnologia
+    const expLabel = createLabel('Experiência: ')
+
+    const id1 = 'expRadio- ' + rowIndex + '.1'
+    const expRadio1 = createInput(id1,'0-2 anos', 'techExp- ' + rowIndex, 'radio' )
+    const expLabel1 = createLabel('0-2 anos', id1)
+
+    const id2 = 'expRadio- ' + rowIndex + '.2'
+    const expRadio2 = createInput(id2,'3-4 anos', 'techExp- ' + rowIndex, 'radio' )
+    const expLabel2 = createLabel('3-4 anos', id2)
+
+    const id3 = 'expRadio- ' + rowIndex + '.3'
+    const expRadio3 = createInput(id3,'5+ anos', 'techExp- ' + rowIndex, 'radio' )
+    const expLabel3 = createLabel('5+ anos', id3)
+
+   const removeRowBtn = document.createElement('button')
+   removeRowBtn.type = 'button'
+   removeRowBtn.innerText = 'Remover'
+   removeRowBtn.addEventListener('click', function(){
+    stackInputs.removeChild(newRow)
+   })
     
     //Usando o método append para guardar os elementos criados, dentro das 'li' e depois, da 'ul'
     newRow.append(
-        techNameLabel, techNameInput
+        techNameLabel, techNameInput, expLabel, expLabel1, expRadio1, expLabel2, expRadio2, expLabel3, expRadio3, removeRowBtn
     )
 
     stackInputs.append(newRow)
 
+})
+
+form.addEventListener('submit', function(ev){
+    ev.preventDefault()
+
+    const fullNameInput = document.getElementById('fullname')
+    const inputRows = document.querySelectorAll('.inputRow')
+
+    let techs = []
+
+    inputRows.forEach(function(row){
+        const techName = document.querySelector('#' + row.id + ' input[name="techName"]').value
+
+        const techExp = document.querySelector('#' + row.id + ' input[type="radio"]:checked').value
+
+        techs.push({name: techName, exp: techExp})
+    })
+
+    const newDev = { fullname: fullNameInput.value, techs: techs}
+
+    developers.push(newDev)
+    alert('Dev cadastrado com sucesso! :D')
+
+    fullNameInput.value = ''
+    inputRows.forEach(function(row){
+        row.remove()
+    })
+    console.log(developers)
 })
